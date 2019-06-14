af5b314fa6786ea64186ba0b3e499603a02f71bf
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2019-06-14 12:45:37 +0200

    Update style.js

--- style.js
+++ style.js
@@ -40,6 +40,7 @@
 				{ name: 'Énfasis Azul con fondo', element: 'span', attributes: { 'class': 'bck-enfasis bck-enfasis-12'} },
 				{ name: 'Énfasis Aguamarina con fondo', element: 'span', attributes: { 'class': 'bck-enfasis bck-enfasis-13'} },
 				{ name: 'Énfasis Subrayado Amarillo', element: 'span', attributes: { 'class': 'bck-enfasis bck-enfasis-14'} },
+				{ name: 'Énfasis Cuaderno Actividades', element: 'span', attributes: { 'class': 'bck-enfasis bck-enfasis-15'} },
 
 
 				{ name: 'Lista ordenada 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
